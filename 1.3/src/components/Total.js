f43aded88ab8818945f3dc1fe1119a3b2b4d94b1
cenacchi2000 <filo.cenacchi@gmail.com>
2021-12-30 20:53:08 +0200

    Part 1.3

---
+++ 1.3/src/components/Total.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Total(props){
+    return(
+        <>
+            <p>Number of exercises {props.part1.exercises +props.part2.exercises + props.part3.exercises}</p>
+        </>
+    )
+}
+
+export default Total;
