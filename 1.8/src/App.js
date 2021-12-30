1de6a2994bbad2997ae2d799d351bb1d66984563
cenacchi2000 <filo.cenacchi@gmail.com>
2021-12-30 20:58:25 +0200

    Part 1.8

---
+++ 1.8/src/App.js
@@ -0,0 +1,60 @@
+import React ,{ useState } from "react";
+
+function Statistics(props) {
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+  const {good, neutral, bad} = props
+
+  useEffect(() => {
+    let avg = (good + bad ) / 3;
+    setAverage(avg.toFixed(2));
+  
+    setPositive(good && ((good / (bad + good ) ) * 100).toFixed(2));
+  }, [good, neutral, bad]);
+  
+  return (
+      <>
+        { good || neutral || bad ?<>
+        <h1>Statistics: </h1>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>All: {good + bad + neutral}</p>
+        <p>Average: {average}</p>
+        <p>Positive: {positive}%</p></>: <p>No feedback given</p>}
+      </>
+  )
+}
+
+function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const countGood = () => {
+    setGood(good +1)
+  }
+
+  const countNeutral = () => {
+    setNeutral(neutral +1)
+  }
+
+  const countBad = () => {
+    setBad(bad +1)
+  }
+
+  return (
+    <div>
+	  <h1>Give Feedback</h1>
+      <button type="button" onClick={countGood}>Good</button>
+      <button type="button" onClick={countNeutral}>Neutral</button>
+      <button type="button" onClick={countBad}>Bad</button>
+      <>
+        <Statistics good={good} bad={bad} neutral={neutral}/>
+      </>
+    </div>
+
+  );
+}
+
+export default App;
