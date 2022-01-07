9bfaad1087aed2d13ad15d9edbf5b60ca67003cf
cenacchi2000 <filo.cenacchi@gmail.com>
2022-01-07 17:27:51 +0200

    Part 4.3

---
+++ 4.3/app/controllers/blog.controller.js
@@ -0,0 +1,35 @@
+const db        = require("../models");
+const Blog      = db.blogs;
+const listHelper  = require('../utils/list_helper');
+
+require('dotenv').config();
+
+let env = process.env;
+
+exports.getBlogs = (req, res) => {
+    Blog.find()
+    .then(async (blogs) => {
+        res.status(200).send(blogs);
+    }).catch(err => {
+        res.status(500).send({message: "Some error occurred while fetching blogs." + err.message})
+    });
+};
+
+exports.getInfo = (req, res) => {
+    const blog = new Blog(req.body)
+ 
+    blog
+        .save()
+        .then(blogs => {
+            res.status(200).send(blogs);
+        }).catch(err => {
+            res.status(500).send({message: "Some error occurred while inserting blogs." + err.message})
+        });
+};
+
+exports.test1 = (req, res) => {
+    const blogs = []
+ 
+    const result = listHelper.dummy(blogs);
+    res.status(200).send({result: result});
+};
---
+++ 4.3/app/utils/list_helper.js
@@ -0,0 +1,4 @@
+module.exports.dummy = (blogs) =>{
+    console.log(blogs);
+    return 1;
+}
