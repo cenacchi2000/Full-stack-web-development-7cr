7f5c0302ff6638bd3a81c1985bf3e29d1d9e58ac
cenacchi2000 <filo.cenacchi@gmail.com>
2022-01-07 17:21:09 +0200

    Part 3.17

---
+++ 3.17/app/controllers/api.controller.js
@@ -0,0 +1,122 @@
+const db        = require("../models");
+const Person    = db.persons;
+
+require('dotenv').config();
+
+let env = process.env;
+const fs = require("fs");
+
+exports.getPersons = (req, res) => {
+    Person.find()
+    .then(async (data) => {
+        res.status(200).send(data);
+    }).catch(err => {
+        res.status(500).send({message: "Some error occurred while fetching persons." + err.message})
+    });
+};
+
+exports.getInfo = (req, res) => {
+    fs.readFile("./app/db/db.json", "utf8", (err, jsonString) => {
+        if (err) {
+            res.status(500).send({message: "Some error occurred " + err})
+        }
+        let data = JSON.parse(jsonString);
+        let msg = "Phonebook has info for "+data.length+" people";
+        res.status(200).send(msg);
+    });
+};
+
+exports.getPersonById = (req, res) => {
+    let id = req.params.id;
+
+    Person.findByIdAndRemove(id).then(_data => { 
+        if (!_data) {
+            return res.status(404).send({
+                message:"Cannot delete Person with id="+id+". Maybe Person was not found!"
+            });
+        } else {
+            return res.status(200).send({
+                message:"Person deleted successfully"
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({message: err.message})
+    });
+};
+
+exports.deletePersonById = (req, res) => {
+    let id = req.params.id;
+
+    Person.findByIdAndRemove(id).then(_data => { 
+        if (!_data) {
+            return res.status(404).send({
+                message:"Cannot delete Person with id="+id+". Maybe Person was not found!"
+            });
+        } else {
+            return res.status(200).send({
+                message:"Person deleted successfully"
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({message: err.message})
+    });
+};
+
+exports.savePersons = (req, res) => {
+    let name = req.body.name;
+    let number = req.body.number;
+
+    if(name === ''){
+        return res.status(400).send({
+            message:"Name can not be blank"
+        });
+    }
+    else if(number === ''){
+        return res.status(400).send({
+            message:"Number can not be blank"
+        });
+    }else{
+        const person = new Person({
+            name: name,
+            number: number,
+        });  
+        
+        person
+        .save(person)
+        .then(data => {
+                res.send(data);
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message:
+                    err.message || "Some error occurred while creating the Person."
+                });
+            });
+    }
+};
+
+exports.updatePersonById = (req, res) => {
+    let id = req.params.id;
+    
+    Person.update({
+        number: number
+    },{
+        where: {id: id}
+    })
+    Person.findByIdAndUpdate(id, req.body, { useFindAndModify: false }).then(data => {
+        if (!data) {
+            return res.status(404).send({
+                message:"Cannot delete Person with id="+id+". Maybe Person was not found!"
+            });
+        } else {
+            return res.status(200).send({
+                message:"Person updated successfully"
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({message: "Some error occurred while updating person."+err.message})
+    });
+};
