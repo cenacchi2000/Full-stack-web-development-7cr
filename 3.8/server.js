fc95b8edbfaf1680b9a57e96129676d7c1ca6dd4
cenacchi2000 <filo.cenacchi@gmail.com>
2022-01-07 16:27:54 +0200

    Part 3.8

---
+++ 3.8/server.js
@@ -0,0 +1,38 @@
+const express = require("express"),
+        morgan = require('morgan'),
+        bodyParser = require("body-parser"),
+        cron = require('node-cron'),
+        app = express();
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Request-Method", "POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+
+
+});
+
+app.use(bodyParser.json());
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :req[content-length] - :response-time ms :body'));
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//include routes
+const router = require("./app/routes")(app);
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Welcome to Phonebook API"
+    })
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server running in port ${PORT}`);
+})
